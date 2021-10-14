aedd3df60d1e97607448153ab73aba3d7e471081
dyaksa <diasnour0395@gmail.com>
2021-10-14 11:17:48 +0700

    add route has valid jwt needed

--- routes/routes.visitor.js
+++ routes/routes.visitor.js
@@ -1,25 +1,31 @@
 const VisitorController = require('../controller/visitor.controller');
 const upload = require('../middleware/upload.middleware');
+const AuthMiddleware = require('../middleware/auth.middleware');
 
 exports.routes = (app) => {
     app.post('/visitor/create', [
+        AuthMiddleware.validJWTNeeded,
         upload.uploadImage,
         VisitorController.create
     ]);
 
     app.get('/visitor/list', [
+        AuthMiddleware.validJWTNeeded,
         VisitorController.get
     ]);
 
     app.get('/visitor/:id/detail', [
+        AuthMiddleware.validJWTNeeded,
         VisitorController.detail
     ]);
 
     app.patch('/visitor/:id/update', [
+        AuthMiddleware.validJWTNeeded,
         VisitorController.update
     ]);
 
     app.delete('/visitor/:id/delete', [
+        AuthMiddleware.validJWTNeeded,
         VisitorController.delete
     ])
 }
